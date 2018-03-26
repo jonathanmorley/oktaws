9db982567dce0b4910eaf350a0d592026f0fd803
Jonathan Morley <jmorley@cvent.com>
2018-03-26 15:22:44 -0400

    Add version prefix to download URL

--- pkg/brew/oktaws-bin.rb
+++ pkg/brew/oktaws-bin.rb
@@ -4,9 +4,9 @@ class OktawsBin < Formula
   homepage "https://github.com/jonathanmorley/oktaws"
 
   if OS.mac?
-      url "https://github.com/jonathanmorley/oktaws/releases/download/#{version}/oktaws-#{version}-x86_64-apple-darwin.tar.gz"
+      url "https://github.com/jonathanmorley/oktaws/releases/download/#{version}/oktaws-v#{version}-x86_64-apple-darwin.tar.gz"
   elsif OS.linux?
-      url "https://github.com/jonathanmorley/oktaws/releases/download/#{version}/oktaws-#{version}-x86_64-unknown-linux-musl.tar.gz"
+      url "https://github.com/jonathanmorley/oktaws/releases/download/#{version}/oktaws-v#{version}-x86_64-unknown-linux-musl.tar.gz"
   end
 
   def install
